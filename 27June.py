512ae0118310f6ed260f03b838c914c8785e9eb9
kiranvkm <52090998+kiranvkm@users.noreply.github.com>
2019-06-28 06:45:17 +0530

    Add files via upload

---
+++ 27June.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#Dictionaries
+dict1 = {'color':'green','points':5}
+print(dict1)
+print(dict1['color'])
+
+
+# In[5]:
+
+
+print(dict1['points'])
+
+
+# In[ ]:
+
+
+
+
