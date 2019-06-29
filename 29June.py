eedabbcd8d0d0361a222bdf1f9f7787afcf712c0
kiranvkm <52090998+kiranvkm@users.noreply.github.com>
2019-06-29 07:52:00 +0530

    Add files via upload

---
+++ 29June.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Looping thyrough dictionaries
+user_0 = {'username':'kiranvkm','fn':'venkata','ln':'mommileti',
+         }
+for key,value in user_0.items():
+    print(f"\n Key : {key}")
+    print(f"\n Value : {value}")
+
+
+# In[3]:
+
+
+for key in user_0.keys():
+    print(f"\nKey : {key}")
+
+
+# In[6]:
+
+
+for val in user_0.values():
+    print(f"\nValue : {val}")
+
+
+# In[ ]:
+
+
+
+
