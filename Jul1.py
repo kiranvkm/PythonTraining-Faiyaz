27e6d5678c9811d996fe19390d0f274ccfb6c778
kiranvkm <52090998+kiranvkm@users.noreply.github.com>
2019-07-01 07:40:46 +0530

    Add files via upload

---
+++ Jul1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Declaring user input
+message = input("what is your firstname\n")
+
+
+# In[4]:
+
+
+print(f"{message} is the first name")
+
+
+# In[5]:
+
+
+print(f"Hello {message} welcome to PT classes")
+
+
+# In[8]:
+
+
+bicycles = ['hero','hercules','bsa']
+print(f"Hello {message}, here is your fav bicycle {bicycles[0]}")
+
+
+# In[10]:
+
+
+age = input("what is ur age\n")
+
+
+# In[12]:
+
+
+print(f"\n your age is : {age}")
+
+
+# In[19]:
+
+
+age > 30
+
+
+# In[20]:
+
+
+#Converting string to Int dtatype
+age_int = int(age)
+print(age_int)
+
+
+# In[18]:
+
+
+age_int > 30
+
+
+# In[21]:
+
+
+age_int < 30
+
+
+# In[ ]:
+
+
+
+
